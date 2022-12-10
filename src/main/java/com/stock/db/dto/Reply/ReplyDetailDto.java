889afc3fc02d3120e7a673cdfa4b60637cc91eab
Yooyeol <duf7040@naver.com>
2022-12-11 03:13:20 +0900

    initial commit

--- src/main/java/com/stock/db/dto/Reply/ReplyDetailDto.java
+++ src/main/java/com/stock/db/dto/Reply/ReplyDetailDto.java
@@ -19,3 +19,24 @@ public class ReplyDetailDto {
 
     private int bno;
 }
+package com.stock.db.dto.Reply;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ReplyDetailDto {
+
+    private int rno;
+
+    private String writer;
+
+    private String content;
+
+    private LocalDateTime writeDate;
+
+    private int mno;
+
+    private int bno;
+}
