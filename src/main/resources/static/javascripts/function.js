05d0a8968f8ef1e714c422ef68ffa4b5644a7cce
Yooyeol <duf7040@naver.com>
2023-01-11 21:06:24 +0900

    UI 업데이트

--- src/main/resources/static/javascripts/function.js
+++ src/main/resources/static/javascripts/function.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     toDateFormat();
     toLocaleFormat();
+    toRateFormat();
 })
 
 function toDateFormat(){
@@ -19,3 +20,26 @@ function toLocaleFormat(){
         num.innerText = parseInt(num.innerText).toLocaleString();
     }
 }
+
+function toRateFormat(){
+    var rates = document.getElementsByClassName("rate");
+    for(var rate of rates){
+        var origin = parseFloat(rate.innerHTML);
+        var newVal = (origin*100).toFixed(2);
+        var newHtml = '';
+        var color = 'black';
+        if(newVal > 0){
+            newHtml = '+' + newVal + '%';
+            color = 'red';
+        }
+        else if(newVal < 0){
+            newHtml =  newVal + '%';
+            color = 'blue';
+        }
+        else{
+            newHtml = '+' + newVal + '%';
+        }
+
+        rate.innerHTML = '<span style = "color :' + color + '">' + newHtml + '</span>';
+    }
+  }
